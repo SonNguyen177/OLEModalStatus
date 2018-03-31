154f36e72dc7bcfc1db456b6352018a71ca1d7eb
Son Nguyen <nguyenhoaisonfat@gmail.com>
2018-03-31 12:04:37 +0700

    Initial Commit

---
+++ OLEModalStatus/OLEModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  OLEModalStatus.h
+//  OLEModalStatus
+//
+//  Created by Son Nguyen on 3/31/18.
+//  Copyright © 2018 Son Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OLEModalStatus.
+FOUNDATION_EXPORT double OLEModalStatusVersionNumber;
+
+//! Project version string for OLEModalStatus.
+FOUNDATION_EXPORT const unsigned char OLEModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OLEModalStatus/PublicHeader.h>
+
+
